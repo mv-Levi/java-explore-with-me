ccde4cb1100f1df43fbb110b7e1c573f658a5ee8
emchik <153514866+mv-Levi@users.noreply.github.com>
2025-04-14 20:26:00 +0300

    Исправил методы StatController

--- stat/stat-server/src/main/java/ru/practicum/controller/StatController.java
+++ stat/stat-server/src/main/java/ru/practicum/controller/StatController.java
@@ -3,8 +3,11 @@ package ru.practicum.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStats;
 import ru.practicum.service.StatService;
@@ -20,15 +23,24 @@ public class StatController {
     private final StatService statService;
 
     @PostMapping("/hit")
-    public void postHit(@RequestBody @Valid EndpointHitDto endpointHitDto) {
+    public ResponseEntity<Void> postHit(@RequestBody @Valid EndpointHitDto endpointHitDto) {
         statService.postHit(endpointHitDto);
+        return new ResponseEntity<>(HttpStatus.CREATED); // → 201
     }
 
     @GetMapping("/stats")
-    public List<ViewStats> getStat(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                                   @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
-                                   @RequestParam(required = false) List<String> uris,
-                                   @RequestParam(defaultValue = "false") Boolean unique) {
+    public List<ViewStats> getStat(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(defaultValue = "false") Boolean unique) {
+
+        if (end.isBefore(start)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "End must be after start");
+        }
+
         return statService.getStat(start, end, uris, unique);
     }
+
+
 }
